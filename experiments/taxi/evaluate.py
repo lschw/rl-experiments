ae786d93e1035218fbf9a2859f2b2696ea3b0ed6
Lukas Schwarz <code@lukasschwarz.de>
2021-02-20 16:52:09 +0100

    Modify Taxi evaluate function

--- experiments/taxi/evaluate.py
+++ experiments/taxi/evaluate.py
@@ -1,32 +1,27 @@
 import numpy as np
 
-def evaluate_policy(env, pi, N_episodes, N_iterations):
+def evaluate_policy(env, pi, N_episodes):
     """Runs certain number of episodes using given policy and calculates
-    mean return. Repeat this N_iterations times and obtain the maximum.
+    mean return.
     """
-    return_mean = []
-    return_max = 0
-    for i_iter in range(1, N_iterations+1):
-        returns = np.zeros(N_episodes)
-        for i_episode in range(N_episodes):
-            done = False
-            state = env.reset()
-            while not done:
-                action = np.argmax(pi[state])
-                state,reward,done,info = env.step(action)
-                returns[i_episode] += reward
-        return_max = np.max([return_max, returns.max()])
-        return_mean.append(returns.mean())
-        print("\rIteration {:>5}".format(i_iter), end="")
-        print(", mean return {:>5.2f}, max return {:>5.2f}".format(
-            returns.mean(), returns.max()), end="")
-        print(", best mean return so far: {:>5.2f}".format(
-            np.max(return_mean)), end="")
-        print(", best max return so far: {:>5.2f}".format(
-            return_max), end="")
+    returns = np.zeros(N_episodes)
+    for i_episode in range(N_episodes):
+        done = False
+        state = env.reset()
+        while not done:
+            action = np.argmax(pi[state])
+            state,reward,done,info = env.step(action)
+            returns[i_episode] += reward
+        print("\rEpisode {:>5}".format(i_episode+1), end="")
+        print(", return {:>7.2f}".format(
+           returns[i_episode]), end="")
+        print(", mean return so far: {:>7.2f}".format(
+            np.mean(returns)), end="")
+        print(", max return so far: {:>7.2f}".format(
+            np.max(returns)), end="")
 
     print()
     print("Maximum return observed within {} episodes: {}".format(
-        N_episodes*N_iterations, return_max))
-    print("Best mean return over {} episodes within {} iterations: {}".format(
-        N_episodes, N_iterations, np.max(return_mean)))
+        N_episodes, np.max(returns)))
+    print("Mean return over {} episodes: {}".format(
+        N_episodes, np.mean(returns)))
--- experiments/taxi/taxi_dp.py
+++ experiments/taxi/taxi_dp.py
@@ -9,8 +9,9 @@ env = gym.make("Taxi-v3")
 
 print("DP policy iteration")
 pi,v = alg.dp_policy_iteration(env, gamma=0.9, tol=1e-3, iter_max=5000)
-evaluate_policy(env, pi, 100, 10000)
+evaluate_policy(env, pi, 1000)
 
 print("DP value iteration")
 pi,v = alg.dp_value_iteration(env, gamma=0.9, tol=1e-3, iter_max=5000)
-evaluate_policy(env, pi, 100, 10000)
+evaluate_policy(env, pi, 1000)
+
